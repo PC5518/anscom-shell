af93f57184cfb55d6ccd2b4548674770efeb2f8c
PC5518 <adityansdsdc@outlook.com>
2026-07-31 23:03:22 +0530

    the use of namespace fs =  std::filesystem

--- std_filesystem.cpp
+++ std_filesystem.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <filesystem>
-
+namespace fs  =  std::filesystem;
 int main() {
-    for (const auto& i : std::filesystem::directory_iterator(".")) {
+    for (const auto& i : fs::directory_iterator(".")) {
         std::cout << i<<std::endl;
     }
 }
