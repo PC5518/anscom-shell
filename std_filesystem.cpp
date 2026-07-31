6d96147c0c2b54420c20771efd81d5ff0e91a98c
PC5518 <adityansdsdc@outlook.com>
2026-07-31 23:40:32 +0530

    countering the bug and the total number of files in a directory.

--- std_filesystem.cpp
+++ std_filesystem.cpp
@@ -2,7 +2,12 @@
 #include <filesystem>
 namespace fs  =  std::filesystem;
 int main() {
+    int count=0; // note: if i will not assign a number the int variable will not asign zero by it's default and therefore it willl use rancom bits
     for (const auto& i : fs::directory_iterator(".")) {
         std::cout << i<<std::endl;
+        count++;
+
     }
+    
+    std::cout <<"the total number of files are: " <<count << std::endl;
 }
