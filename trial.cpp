5a6f61e0a788d35d06f2c63f95e2b37ef59bcd21
PC5518 <adityansdsdc@outlook.com>
2026-07-31 22:32:15 +0530

    chaning the name

---
+++ trial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+int main() {
+    while (true) {
+        std::cout<< "anscom: $   " ;
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "about") {
+            std::cout <<"this is a pre maded version of anscom terminal shell" << std::endl;
+        }
+        else if (input == "exit") {
+            break;
+        }
+        else {
+            std::cout <<"the command " << input << " not found" << std::endl;
+        }
+    }
+}
+// SAMPLE COMMIT
