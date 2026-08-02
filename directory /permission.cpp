fefa4f8c2bf213d00169b2eb6b360a2b2ee00262
PC5518 <adityansdsdc@outlook.com>
2026-08-02 17:11:43 +0530

    git issues recovery

--- directory /permission.cpp
+++ directory /permission.cpp
@@ -4,14 +4,14 @@ namespace fs  =  std::filesystem;
 
 int main() {
     int count=0; // note: if i will not assign a number the int variable will not asign zero by it's default and therefore it willl use rancom bits
-    for (const auto& i : fs::directory_iterator(".")) {
+    for (const auto& i : fs::directory_iterator("/root")) {
         std::cout << i<<std::endl;
         count++;
 
     }
     int total=0;
     std::cout << "###########################################################"<<std::endl;
-    for (const auto& j: fs::recursive_directory_iterator(".")) {
+    for (const auto& j: fs::recursive_directory_iterator("/root")) {
         std::cout <<j <<std::endl;
         
         total++;
@@ -24,20 +24,15 @@ int main() {
     int files=0;
     int folders=0;
     
-    for (const auto& i : fs::recursive_directory_iterator("."))  {
+    for (const auto& i : fs::recursive_directory_iterator("/root"))  {
         if (fs::is_regular_file(i)) {
             files ++;
         } else {
             folders++;
         }
     }
-<<<<<<< HEAD
-    std::cout << files<<std::endl;
-    std::cout << folders <<std::endl;
-=======
     std::cout <<"files: " <<files<<std::endl;
     std::cout << "folders: " <<folders <<std::endl;
->>>>>>> eb51bbb (git issues recovery)
     
 
 }
