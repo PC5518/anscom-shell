bbef35e4d2ab4c38b03c4bcf825a64644279060b
PC5518 <adityansdsdc@outlook.com>
2026-08-02 15:42:02 +0530

    logic for finding out files and folders

--- std_filesystem.cpp
+++ std_filesystem.cpp
@@ -21,4 +21,17 @@ int main() {
     // note: this std::filesystem::recursive_directory_iterator 
     // the issue with the recursive directory iterator is that: recursive_directory_iterator doesn't just count files — it treats every entry (both files and folders) as one item in the iteration.
     // solution: in the next file:
+    int files=0;
+    int folders=0;
+    
+    for (const auto& i : fs::recursive_directory_iterator("."))  {
+        if (fs::is_regular_file(i)) {
+            files ++;
+        } else {
+            folders++;
+        }
+    }
+    std::cout << files<<std::endl;
+    std::cout << folders <<std::endl;
+
 }
