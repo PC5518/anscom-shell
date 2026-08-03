3de877c1f4b7dfb3bac5c84e98cc16f7376666b4
PC5518 <adityansdsdc@outlook.com>
2026-08-03 15:59:49 +0530

    file_size founder corrected code

--- directory/file_size.cpp
+++ directory/file_size.cpp
@@ -4,7 +4,7 @@ namespace fs = std::filesystem;
 int main() {
     for (const auto& i :fs::directory_iterator(".") ) {
         if (fs::is_regular_file(i)) {
-            i.file_size();
+            std::cout << i<<": "<<fs::file_size(i) <<std::endl;
         } else {
             continue;
         }
