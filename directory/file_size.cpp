29ddf615734c01e9c3ea8aa1651707121f78a466
PC5518 <adityansdsdc@outlook.com>
2026-08-03 16:06:30 +0530

    the file size data extractor if the we took recursive directory scanner

--- directory/file_size.cpp
+++ directory/file_size.cpp
@@ -2,7 +2,7 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 int main() {
-    for (const auto& i :fs::directory_iterator(".") ) {
+    for (const auto& i :fs::recursive_directory_iterator(".") ) {
         if (fs::is_regular_file(i)) {
             std::cout << i<<": "<<fs::file_size(i) <<std::endl;
         } else {
