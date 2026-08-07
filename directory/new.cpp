4731be19a681dfb171a6032af9114288a2c399ba
PC5518 <adityansdsdc@outlook.com>
2026-08-07 15:47:29 +0530

    new test commit

---
+++ directory/new.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+#include <filesystem>
+namespace  fs =std::filesystem;
+int main() {
+    for (const auto& i:  fs::directory_iterator(".")) {
+        std::cout << i<<std::endl;
+    }
+}
