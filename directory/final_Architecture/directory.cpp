a70c22092c5c9b5469ce55a43fe0d3c3819be2af
PC5518 <adityansdsdc@outlook.com>
2026-08-08 18:20:21 +0530

    updated directory analyser code with linux windows sudo support.

--- directory/final_Architecture/directory.cpp
+++ directory/final_Architecture/directory.cpp
@@ -8,7 +8,7 @@ int main() {
     int folders =0;
     int denied_count=0;
     std::error_code error_data;
-    for (const auto& i : fs::recursive_directory_iterator("/root", fs::directory_options::skip_permission_denied)) {
+    for (const auto& i : fs::recursive_directory_iterator(".", fs::directory_options::skip_permission_denied)) {
         std::cout << i<<" " ;
         bool isdir = fs::is_directory(i,error_data);  // note  in this  fs::directory_options::skip_permission_denied) the loop body is also ecxecuted the skip permission denied only make it silent and makes the code throw no error.
         // why this code doesn't work:  The option silences the error: By using fs::directory_options::skip_permission_denied, you tell the iterator to completely skip permission-denied items in the background.
@@ -29,4 +29,13 @@ int main() {
     std::cout <<"total files: " << files  <<std::endl;
     std::cout << "total folders: " << folders<<std::endl;
     std::cout << "total files, folders and denied files combined: " << folders+files + denied_count<<std::endl;
+#if defined(_WIN32) 
+    std::cout <<"for the most accurate results on MS Windows run in the administrator mode or with sudo. you might sudo with wsl for the best results. wsl: sudo ./sampleexefile" <<std::endl;
+#elif defined(__linux__) 
+    std::cout << "For the most accurate results on Linux machine run use with sudo.  Bash: sudo ./sampleexefile"<<std::endl;
+#else
+    std::cout <<"run the shell with admin access for best results" <<std::endl;
+#endif 
+
+    return 0;
 }//std::filesystem::directory_options::skip_permission_denied does not skip your loop's code. It tells the directory iterator to silently ignore access-denied folders and move straight to the next readable file or folder without throwing an error. Your loop body runs normally for every valid item.
